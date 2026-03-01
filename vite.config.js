0c4bc9c1cc9d228bbecb1cab199bf6e26b082ba6
shruu1403 <shruti.gupta01412.com>
2026-03-01 14:46:17 +0530

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/my-app/",   // ⚠️ replace with your repo name
-  plugins: [react()],
+  base: "/my-app/",  
+  plugins: [react(), tailwindcss()],
 })
